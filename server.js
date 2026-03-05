f436c55d0dc38b946124a311d8a3fce4e89de888
mizandarii <kristiina.parri@ivkhk.ee>
2026-03-05 10:38:58 +0200

    added package.json

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express'); 
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+const NIMI = process.env.MY_NAME || 'Tundmatu   nimi';
+
+app.get('/api/info', (req, res) => {
+    res.status(200).json({
+        mission: "Iseseisev deploimine edukas",
+
+        meeskond: NIMI,
+        eeg: new Date().toISOString()
+
+    });
+});
+
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`API Server töötab selle pordi peale ${PORT}`);
+});
